ef85e17ac9631967f306d759338a712e8e1287e9
Snesnopic <snesnopic@gmail.com>
2026-01-31 11:53:58 +0100

    Woops wrong sha256

--- Formula/vbrfix.rb
+++ Formula/vbrfix.rb
@@ -2,7 +2,7 @@ class Vbrfix < Formula
   desc "Repair MP3 Variable Bit Rate (VBR) headers to fix seek issues"
   homepage "https://github.com/Snesnopic/vbrfix"
   url "https://github.com/Snesnopic/vbrfix/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "83223f3b281d950e2bd415b543f903b55cecb767"
+  sha256 "0c5b9b74be1c41741b33b44c4965040f72ddb26b1b4407fd657f08c6fee9f7b1"
   license "GPL-2.0-or-later"
 
   depends_on "cmake" => :build
